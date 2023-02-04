100881f434d8d7869e59a6631ed77e3f7dcad338
anousheh21 <anousheh21@gmail.com>
2023-02-04 09:58:56 +0000

    Clears input fields, displays user input

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const yInput = document.querySelector('.inputy');
 
 const button = document.querySelector('.submit');
 const output = document.querySelector('.output');
+const displayCartesian = document.querySelector('.displayCartesian');
+const showIcon = document.querySelector('.showIcon');
 
 button.addEventListener('click', () => {
     let x = parseFloat(xInput.value);
@@ -11,6 +13,8 @@ button.addEventListener('click', () => {
     let modulus = 0;
     if(xInput.value=='' || yInput.value=='') {
         output.textContent = 'Values Required'
+        displayCartesian.textContent = '';
+        showIcon.textContent = '';
     } else {
         let pythag = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
         if(Number.isInteger(pythag)) {
@@ -21,6 +25,10 @@ button.addEventListener('click', () => {
         let argz = Math.atan(y/x).toFixed(3);
         let modarg = `${modulus} cos(${argz})  +  ${modulus}i sin(${argz})`
         output.textContent = modarg;
+        showIcon.textContent = 'arrow_downward';
+        xInput.value = '';
+        yInput.value = '';
+        displayCartesian.textContent = `${x} + ${y}i`
     }
 
 });
