bf9580ea829299aebe36a51cbac93dd166dc60ab
anousheh21 <anousheh21@gmail.com>
2023-02-04 13:52:56 +0000

    Converts from modulus argument form to cartesian form

---
+++ ModulusArgumentToCartesian/scriptmatoc.js
@@ -0,0 +1,36 @@
+const inputMod = document.querySelector('.inputmod');
+const inputArg = document.querySelector('.inputarg');
+const button = document.querySelector('.submit');
+const displayModArg = document.querySelector('.displayModarg');
+const output = document.querySelector('.output');
+const showIcon = document.querySelector('.showIcon');
+
+button.addEventListener('click', () => {
+    let n = parseFloat(inputMod.value);
+    let theta = parseFloat(inputArg.value);
+    let x = 0;
+    let y = 0;
+    if (inputMod.value=='' || inputArg.value=='') {
+        output.textContent = 'Values Required';
+        displayModArg.textContent = '';
+        showIcon.textContent='';
+    } else {
+        x = n*(Math.cos(theta));
+        y = n*(Math.sin(theta));
+        if (Number.isInteger(x)) {
+            x=x;
+        } else {
+            x=x.toFixed(2);
+        }
+        if (Number.isInteger(y)) {
+            y=y;
+        } else {
+            y=y.toFixed(2);
+        }
+        inputMod.value = '';
+        inputArg.value = '';
+        output.textContent=`${x} + ${y}i`;
+        showIcon.textContent='arrow_downward';
+        displayModArg.textContent=`${n}cos(${theta}) + ${n}i sin (${theta})`;
+    }
+})
